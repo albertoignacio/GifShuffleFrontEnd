9d5c64d6bfc7497e5b06c2ccc5ea390d099c3b83
Alberto Ignacio Ortega <ortegalbertoi@gmail.com>
2026-06-11 11:15:34 -0300

    se agrego boton incluir usuario

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,48 +1,49 @@
 ﻿export interface AuthResponse {
-  token: string
-  name: string
-  lastName: string
-  email: string
+  token: string;
+  name: string;
+  lastName: string;
+  email: string;
 }
 
 export interface LoginRequest {
-  email: string
-  password: string
+  email: string;
+  password: string;
 }
 
 export interface RegisterRequest {
-  name: string
-  lastName: string
-  email: string
-  password: string
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
 }
 
 export interface FriendResponse {
-  id: string
-  name: string
-  lastName: string
-  email: string
+  id: string;
+  name: string;
+  lastName: string;
+  email: string;
 }
 
 export interface CreateFriendRequest {
-  name: string
-  lastName: string
-  email: string
+  name: string;
+  lastName: string;
+  email: string;
 }
 
 export interface UpdateFriendRequest {
-  name: string
-  lastName: string
-  email: string
+  name: string;
+  lastName: string;
+  email: string;
 }
 
 export interface ShuffleRequest {
-  friendIds: string[]
-  giftAmount: number
+  friendIds: string[];
+  giftAmount: number;
+  includeCurrentUser?: boolean;
 }
 
 export interface ShuffleResponse {
-  shuffled: boolean
-  participantCount: number
-  giftAmount: number
+  shuffled: boolean;
+  participantCount: number;
+  giftAmount: number;
 }
